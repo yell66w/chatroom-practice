ffdbd8ff05c9ca9dca8bcbd7a147aa2e300f962f
Ryan Tillaman <37836505+yellow-cyber@users.noreply.github.com>
2020-11-10 10:05:50 +0800

    source purge

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+    <div>
+      App
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
