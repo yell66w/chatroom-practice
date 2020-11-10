7af504bbee345edd8697f02525f1b11fb043bad7
Ryan Tillaman <37836505+yellow-cyber@users.noreply.github.com>
2020-11-10 16:11:12 +0800

    added scrollbar library

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import MainContainer from './containers/MainContainer'
-
+import { Scrollbars } from 'react-custom-scrollbars';
 const App = () => {
   return (
-    <div className="h-screen">
-      <Navbar/>
-      <MainContainer />
+    <Scrollbars style={{ width: 500, height: 300 }}>
+         <div className="h-screen">
+          <Navbar/>
+          <MainContainer />
     </div>
+    </Scrollbars>
+   
   )
 }
 
