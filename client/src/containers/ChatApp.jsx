dfb8319641a8142d282aaeae270acc0000303455
Ryan Tillaman <37836505+yellow-cyber@users.noreply.github.com>
2020-11-11 12:20:58 +0800

    Register/Login
    Added sweetalert

--- client/src/containers/ChatApp.jsx
+++ client/src/containers/ChatApp.jsx
@@ -1,10 +1,6 @@
-import React from 'react'
-const ChatApp = ({children}) => {
-    return (
-        <div className="flex flex-col min-h-screen h-screen">
-            {children}
-        </div>
-    )
-}
+import React from "react";
+const ChatApp = ({ children }) => {
+  return <div className="flex flex-col min-h-screen h-screen">{children}</div>;
+};
 
-export default ChatApp
+export default ChatApp;
