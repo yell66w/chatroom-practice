bc116f6f547479fe541f4dd829c7967ef602a330
Ryan Tillaman <37836505+yellow-cyber@users.noreply.github.com>
2020-11-12 11:34:54 +0800

    implemented sockets

--- server/index.js
+++ server/index.js
@@ -1,35 +1,28 @@
-const express = require('express')
-const severController = require('./controllers/indexController')
-const mongoose = require('mongoose')
-const jwt = require('jsonwebtoken')
-const app = express()
-const secretkey = 'chihuahua'
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const port = 4000
+const express = require("express");
+const severController = require("./controllers/indexController");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const app = express();
+const secretkey = "chihuahua";
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const port = 4000;
+const http = require("http");
+const server = http.createServer(app);
 //fire controllers
-app.use(bodyParser.json()) //nangalimtan nim
-app.use(cors()) //FOR CROSS-ORIGIN
-severController(app,jwt, mongoose , secretkey)
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.use(bodyParser.json()); //nangalimtan nim
+app.use(cors()); //FOR CROSS-ORIGIN
+severController(app, jwt, mongoose, secretkey);
+
+const io = require("socket.io")(server);
 // this
-const io = require('socket.io')(3000)
-io.on('connection', socket => {
+io.on("connection", (socket) => {
+  socket.emit("socID", socket.id);
   // talking to another person
-  socket.on('chatting-other',( username , otherName ,message) => {
-    socket.in(otherName).emit({
-      sender: username,
-      message: message
-    })
-  })
-  let group = { group: 'dummygroup' }
-  //group chat
-  socket.on('chatting-to-group',( username , groupName , message) => {
-    socket.in(groupName).emit({
-      sender: username,
-      message: message
-    })
-  })
-})
+  socket.on("send message", (body) => {
+    io.emit("message", body);
+  });
+});
+server.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
