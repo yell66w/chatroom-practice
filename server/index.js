82b0be7a9c085d5245df524c8e5921cc698733cf
Ryan Tillaman <37836505+yellow-cyber@users.noreply.github.com>
2020-11-12 13:14:00 +0800

    idk

--- server/index.js
+++ server/index.js
@@ -13,11 +13,15 @@ const server = http.createServer(app);
 app.use(bodyParser.json()); //nangalimtan nim
 app.use(cors()); //FOR CROSS-ORIGIN
 severController(app, jwt, mongoose, secretkey);
-
+const users = {};
 const io = require("socket.io")(server);
 // this
 io.on("connection", (socket) => {
   socket.emit("socID", socket.id);
+  socket.on("new-user", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-connected", name);
+  });
   // talking to another person
   socket.on("send message", (body) => {
     io.emit("message", body);
